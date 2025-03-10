ea27e4354c2d841741e1804293ea23236426e420
Ernest Roca <e.roca@stuart.com>
2025-03-10 16:38:54 +0100

    added new test

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -2,4 +2,8 @@ describe('Test Jest', () => {
     it('should return true', () => {
         expect(true).toBe(true);
     });
+
+    it('should return true', () => {
+        expect(false).toBe(false);
+    });
 });
