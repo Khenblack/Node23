b84aafe48e2bd7e5daa255d705b4f89dbeb00f4b
Ernest Roca <e.roca@stuart.com>
2025-03-12 12:34:08 +0100

    Return success

--- .github/actions/avro-checker/index.js
+++ .github/actions/avro-checker/index.js
@@ -28,7 +28,7 @@ const read_avro_schemas = () => {
 
     core.notice(`Finished reading Avro schemas. Schemas ${JSON.stringify(schemas)}`);
 
-    process.exit(core.ExitCode.Failure);
+    process.exit(core.ExitCode.Success);
 };
 
 read_avro_schemas();
