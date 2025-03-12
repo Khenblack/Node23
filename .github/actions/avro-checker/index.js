cd2259499704464508ae29d086239709f6d78ee0
Ernest Roca <e.roca@stuart.com>
2025-03-12 11:53:24 +0100

    update schema_dir

--- .github/actions/avro-checker/index.js
+++ .github/actions/avro-checker/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const read_avro_schemas = () => {
     const schemas = [];
-    const schema_dir = path.join(__dirname, 'avro');
+    const schema_dir = path.join(process.cwd(), 'avro');
     const files = fs.readdirSync(schema_dir);
     files.forEach((file) => {
         if (file.endsWith('.avsc')) {
