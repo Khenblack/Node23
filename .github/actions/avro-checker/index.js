908114ac649a91d7519c3649239f4eec40b9b6f2
Ernest Roca <e.roca@stuart.com>
2025-03-12 12:09:20 +0100

    added custom action

--- .github/actions/avro-checker/index.js
+++ .github/actions/avro-checker/index.js
@@ -1,7 +1,12 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const exec = require('@actions/exec');
+
 const fs = require('fs');
 const path = require('path');
 
 const read_avro_schemas = () => {
+    core.notice('Reading Avro schemas');
     const schemas = [];
     const schema_dir = path.join(process.cwd(), 'avro');
     const files = fs.readdirSync(schema_dir);
